9b7f8f1038972d5a04acdea2ae1cc0bbfd096b8e
JavierValdez <jasv1411@gmail.com>
2023-06-27 21:29:20 -0600

    aa

--- src/main/java/com/usuarios/usuarios/Dto/Sumatoria.java
+++ src/main/java/com/usuarios/usuarios/Dto/Sumatoria.java
@@ -5,23 +5,18 @@
  */
 package com.usuarios.usuarios.Dto;
 
-import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
  *
- * @author fasp9
+ * @author aesestrad
  */
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class BeneficioDto {
-    private Integer id_ingreso;
-    private Integer id_cuenta;
-    private String usuario_agricultor;
-    private String matricula_autorizada;
-    private String licencia_autorizada;
-    //private Date fecha_asignacion;
+public class Sumatoria {
+    private Integer sumatoriaPeso;
+    private Integer peso_total_de_envio; 
 }
